ca6d77e62427c5807e5d790f0bc7119f7079a5eb
Eric Winnington <eric.in.the.net@gmail.com>
2020-10-08 14:25:14 +0000

    cli-redis done with codespaces

---
+++ cli-redis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StackExchange.Redis; 
+
+namespace cli_redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+            IDatabase db = redis.GetDatabase();
+            db.StringSet("key1", "Hello world from redis"); 
+            Console.WriteLine(db.StringGet("key1")); 
+
+            redis.Close(); 
+        }
+    }
+}
